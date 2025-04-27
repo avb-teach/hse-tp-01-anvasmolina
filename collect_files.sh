7d83e57a64fba7adf558359787df745ad3eb62a2
Анастасия <smolinaa06@mail.ru>
2025-04-27 15:33:12 +0300

    9

--- collect_files.sh
+++ collect_files.sh
@@ -8,6 +8,26 @@ fi
 input_dir="$1"
 output_dir="$2"
 
+copying() {
+  local source_file="$1"
+  local dest_file="$output_dir/$(basename "$source_file")"
+  local cnt=1
+
+  while [ -e "$dest_file" ]; do
+      dest_file="$output_dir/$(basename "$source_file" | cut -d '.' -f 1)_${cnt}.${source_file##*.}"
+      cnt=$((cnt + 1))
+  done
+  cp "$source_file" "$dest_file"
+}
+
+for file in $(find "$input_dir" -type f); do
+  copying "$file"
+done
+
+printf "Файлы успешно скопированы из '%s' в '%s'.\n" "$input_dir" "$output_dir"
+exit 0
+
+
 
 
 

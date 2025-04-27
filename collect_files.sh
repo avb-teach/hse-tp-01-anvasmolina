807ea7ac673a29372b31a47d229b7c0e5b5b7bd5
Анастасия <smolinaa06@mail.ru>
2025-04-27 15:17:58 +0300

    6

--- collect_files.sh
+++ collect_files.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 
-# Проверяем аргументы запуска
 if test "$#" -ne 2; then
     printf "\nОшибка: неправильное количество аргументов.\nПравильно вызывать так:\n\t%s <входная_директория> <выходная_директория>\n\n" "$0"
     exit 1
 fi
+
+input_dir="$1"
+output_dir="$2"
+
+[[ ! -d "$output_dir" ]] && mkdir -p "$output_dir"
+
+copying() {
+  local source_file="$1"
+  local dest_file="$output_dir/$(basename "$source_file")"
+  local cnt=1
+
+  while [ -e "$dest_file" ]; do
+      dest_file="$output_dir/$(basename "$source_file" | cut -d '.' -f 1)_${cnt}.${source_file##*.}"
+      cnt=$((cnt + 1))
+  done
+  cp "$source_file" "$dest_file"
+}
+
+for file in $(find "$input_dir" -type f); do
+  copying "$file"
+done
+
+printf "Файлы успешно скопированы из '%s' в '%s'.\n" "$input_dir" "$output_dir"
+exit 0
+
+
+
+
+
